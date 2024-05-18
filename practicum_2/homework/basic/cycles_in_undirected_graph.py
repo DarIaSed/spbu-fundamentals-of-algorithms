8d73d4e4cd19d33a77b172d6a7123eb1c0fc3752
DarIaSed <149616900+DarIaSed@users.noreply.github.com>
2024-05-18 15:20:02 +0500

    Update cycles_in_undirected_graph.py

--- practicum_2/homework/basic/cycles_in_undirected_graph.py
+++ practicum_2/homework/basic/cycles_in_undirected_graph.py
@@ -1,36 +1,36 @@
+import os
+
 import networkx as nx
 
 
-def has_cycles(G: nx.Graph, v: str, prev_v: str, visited: list[bool]) -> None:
-    global flag
-
-    if not visited[int(v)]:
-        visited[int(v)] = True
-    else:
-        flag = True
-        return
-
-    for neighbor in G[v]:
-        if neighbor != prev_v:
-            has_cycles(G, neighbor, v, visited)
-
-
 TEST_GRAPH_FILES = [
-    "graph_1_wo_cycles.edgelist.txt",
-    "graph_2_w_cycles.edgelist.txt",
+    "graph_1_wo_cycles.edgelist",
+    "graph_2_w_cycles.edgelist",
 ]
+
+
+def has_cycles(g: nx.Graph):
+    visited = set()
+
+    def dfs(node, parent):
+        visited.add(node)
+        for neighbor in g[node]:
+            if neighbor not in visited:
+                if dfs(neighbor, node):
+                    return True
+            elif neighbor != parent:
+                return True
+        return False
+
+    for node in g.nodes():
+        if node not in visited:
+            if dfs(node, None):
+                return True
+    return False
 
 if __name__ == "__main__":
     for filename in TEST_GRAPH_FILES:
-        # Load the graph
-        G = nx.read_edgelist(f"C://Users//User//Алгоритмы//{filename}", create_using=nx.Graph)
-
-        # Output whether it has cycles
-        flag = False
-        visited = [False] * len(G)
-        has_cycles(G, "0", "0", visited)
-        
-        if flag:
-            print(f"Graph {filename} has cycles")
-        else:
-            print(f"Graph {filename} has no cycles") 
+        G = nx.read_edgelist(
+            os.path.join("practicum_2", "homework", filename), create_using=nx.Graph
+        )
+        print(f"Graph {filename} has cycles: {has_cycles(G)}")
