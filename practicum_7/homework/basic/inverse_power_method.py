ffbb85872135bce9308583dec32486f169139b9e
DarIaSed <149616900+DarIaSed@users.noreply.github.com>
2024-05-18 21:28:05 +0500

    Update inverse_power_method.py

--- practicum_7/homework/basic/inverse_power_method.py
+++ practicum_7/homework/basic/inverse_power_method.py
@@ -24,10 +24,10 @@ def inverse_power_method(A: NDArrayFloat, n_iters: int) -> NDArrayFloat:
 if __name__ == "__main__":
     A = np.array(
         [
-        [4.0, 1.0, -1.0, 2.0],
-        [1.0, 4.0, 1.0, -1.0],
-        [-1.0, 1.0, 4.0, 1.0],
-        [2.0, -1.0, 1.0, 1.0],
+            [4.0, 1.0, -1.0, 2.0],
+            [1.0, 4.0, 1.0, -1.0],
+            [-1.0, 1.0, 4.0, 1.0],
+            [2.0, -1.0, 1.0, 1.0],
         ]
     )
     eigvals = inverse_power_method(A, n_iters=10)
